9b5b1c9a256edcb0e9628f0b9237ffb4357963a5
Jeevan Francis <jeevan.francis@student.manchester.ac.uk>
2026-03-27 09:31:24 +0000

    ends session and logs user out

--- php/logout.php
+++ php/logout.php
@@ -6,7 +6,7 @@ session_destroy();
 echo
 " <script> 
 alert('Thank you for staying with us! '); 
-window.location.href='../html/loginmasuk.php'; 
+window.location.href='../html/login.php'; 
 </script> 
 "; 
 ?>
