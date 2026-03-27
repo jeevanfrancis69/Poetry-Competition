e6ac1cc598678c4e9cc2b6c0b64987dd36290018
Jeevan Francis <jeevan.francis@student.manchester.ac.uk>
2026-03-27 09:45:44 +0000

    Remove redundant files

--- Safety.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    
-    if(!isset($_SESSION['usernamePeserta'])){ 
-        header("Location: http://localhost/PertandinganApex/html/loginmasuk.php"); 
-    } 
-?>
--- js/AddPesertaCSV.js
+++
@@ -1,7 +0,0 @@
-var modal = document.getElementById("modal");
-
-window.onclick = function(event) {
-    if(event.target == modal) {
-      modal.style.display = "none";
-    }
-}
