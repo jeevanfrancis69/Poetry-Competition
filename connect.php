96e1d5b5e2c5f0b08bdc5e46baebf147e277a82d
Jeevan Francis <jeevan.francis@student.manchester.ac.uk>
2026-03-27 09:32:16 +0000

    Main connection file to database

--- connect.php
+++ connect.php
@@ -1,9 +1,8 @@
 <?php
-$servername = "localhost";
+$servername = '127.0.0.1';
 $username = "root";
 $password = "root";
 $database = "dbsajak";
 
 $con = mysqli_connect($servername, $username, $password, $database);
 
-#ip address location - http://172.31.140.97/
